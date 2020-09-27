3d69fb10654369154be077253afbcf142f5b4f54
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-27 21:19:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,9 @@ class Scraper
     students
   end
   def self.scrape_profile_page(profile_url)
-    
+      student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
   end
 
 end
