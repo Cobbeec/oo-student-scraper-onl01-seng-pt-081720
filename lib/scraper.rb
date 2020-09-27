fb60a2ba75b0557e2cef53196a94785e8ac93bfb
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-27 21:08:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,14 +3,7 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-   student_hash = []
-   html = Nokogiri::HTML(open(index_url))
-   html.css(".student-card").collect do |student| 
-     hash = {
-       name: student.css()
-     }
-  end
+  
 
   def self.scrape_profile_page(profile_url)
     
